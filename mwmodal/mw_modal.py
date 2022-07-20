adc21311ad7468c595e852e17461cab3914edb17
Ivan Tomac <itomac80@gmail.com>
2022-07-20 16:54:16 +0200

    Polishing of code

--- mwmodal/mw_modal.py
+++ mwmodal/mw_modal.py
@@ -33,19 +33,19 @@ class MorletWaveModal(object):
 
     def identify_natural_frequency(self, omega):
         
-        N_ef = free_response.size
-        k_max = int(omega * N_ef / (2*np.pi*fs))
+        N_ef = self.free_response.size
+        k_max = int(omega * N_ef / (2*np.pi*self.fs))
 
         if self.k_hi > k_max:
             self.k_hi = int(k_max * 0.9)
             self.num_k = 7
-        if k_hi - self.k_lo + 1 < self.num_k:
-            self.num_k = k_hi - self.k_lo + 1
+        if self.k_hi - self.k_lo + 1 < self.num_k:
+            self.num_k = self.k_hi - self.k_lo + 1
 
         self.k = np.linspace(self.k_lo, self.k_hi, self.num_k, dtype=int)
 
         self.find_natural_frequencies(omega)
-        self.mw_integrate()
+        self.morlet_wave_integrate()
 
         omega_identified = np.mean(self.omega_id[self.mask])
         return omega_identified
@@ -61,7 +61,7 @@ class MorletWaveModal(object):
             raise Exception(f'Optimizer returned false:\n{damp.message}.')
         return damp.x[0]
 
-    def identify_amp_pha(self, damping):
+    def identify_amplitude_phase(self, damping):
 
         ######### Amplitude #########
         amp_test = get_amplitude(self.k[self.mask], self.n_1, damping, \
@@ -93,10 +93,10 @@ class MorletWaveModal(object):
         #         ph = np.nan
         phi_test = 0.1
         phase = least_squares(cost_fun_phase, x0=phi_test, method='trf', bounds=(-np.pi, np.pi),  \
-                    args=(self.k[self.mask], np.angle(identifier.integral[0, self.mask])))
+                    args=(self.k[self.mask], np.angle(self.integral[0, self.mask])))
         if phase.cost > 1:
             phase = least_squares(cost_fun_phase, x0=-phi_test, method='trf', bounds=(-np.pi, np.pi),  \
-                    args=(self.k[self.mask], np.angle(identifier.integral[0, self.mask])))
+                    args=(self.k[self.mask], np.angle(self.integral[0, self.mask])))
         if not(phase.success):
             raise Exception(f'Optimizer returned false for phase:\nAmplitude: {amp.x[0]}\n{phase.message}.')
 
@@ -109,7 +109,7 @@ class MorletWaveModal(object):
             self.omega_id[i] = identifier.find_natural_frequency(omega, self.n_1, k_)
         return None
 
-    def mw_integrate(self):
+    def morlet_wave_integrate(self):
         if self.omega_id is None:
             raise Exception(f'Natural frequencies not identified.')
 
@@ -133,17 +133,18 @@ class MorletWaveModal(object):
             raise Exception(f'Number of points is lower then 3, {np.sum(self.mask)}.')
         return None
 
-    def analyze_ratio(self, M_num):
-        k_lim = int(k_limit_theoret(self.n_1, self.damp_lim[1]))
+    def analyze_ratio(self, M_tilde):
+        k_lim = int(k_limit_theoretical(self.n_1, self.damp_lim[1]))
         for i, k_ in enumerate(self.k):
             if k_ <= k_lim:
                 test = cost_fun_damping(self.damp_lim[1], 0, self.n_1, self.n_2, k_)
-                if M_num[i] > test:
-                    M_num[i] = 1
-        d_lim = damping_limit_theoret(self.n_1, self.k_hi)
-        M_lim_lo = low_M_limit((self.n_1, self.n_2))
+                if M_tilde[i] > test:
+                    M_tilde[i] = 1
+        d_lim = damping_limit_theoretical(self.n_1, self.k_hi)
+        # M_lim_lo = low_M_limit((self.n_1, self.n_2))
+        M_lim_lo = cost_fun_damping(self.damp_lim[0], 0, self.n_1, self.n_2, self.k_lo)
         M_lim_hi = cost_fun_damping(d_lim, 0, self.n_1, self.n_2, self.k_hi)
-        mask = np.logical_and(M_num > M_lim_lo, M_num < M_lim_hi)
+        mask = np.logical_and(M_tilde > M_lim_lo, M_tilde < M_lim_hi)
         return mask
 
 def morlet_wave(fs, omega, k, n):
@@ -170,7 +171,7 @@ def morlet_wave(fs, omega, k, n):
     
     return np.pi**-0.25 * s**-0.5 * np.exp(-0.5*t**2 + 1j*eta*t)
 
-def damping_limit_theoret(n, k):
+def damping_limit_theoretical(n, k):
     kapa = 8*np.pi*k / n**2
     d1 = 0.5*(-kapa + np.sqrt(kapa**2 + 4))
     d2 = 0.5*(-kapa - np.sqrt(kapa**2 + 4))
@@ -180,42 +181,42 @@ def damping_limit_theoret(n, k):
         return d2
     return d1, d2
 
-def k_limit_theoret(n, d):
+def k_limit_theoretical(n, d):
     kapa = n**2 / (8*np.pi*d)
     return kapa * np.sqrt(1 - d**2)
 
-def low_M_limit(n):
-    return erf(0.25*n[0]) / erf(0.25*n[1]) * np.sqrt(n[1] / n[0])
+# def low_M_limit(n):
+#     return erf(0.25*n[0]) / erf(0.25*n[1]) * np.sqrt(n[1] / n[0])
 
 def max_N(k_max, omega, fs):
     return int(2 * k_max * np.pi / omega * fs) + 1
 
-def cost_fun_damping(d, M_num, n_1, n_2, k):  # vektoriziran za eMW2
-    const = 2 * k * np.pi * d / np.sqrt(1 - d**2)
+def cost_fun_damping(damp, M_tilde, n_1, n_2, k):  # vektoriziran za eMW2
+    const = 2 * k * np.pi * damp / np.sqrt(1 - damp**2)
     n = np.array([n_1, n_2], dtype=object)
     g_1 = 0.25 * n
     g_2_0 = const / n[0]
     g_2_1 = const / n[1]
-    G = erf(g_1[0] - g_2_0) + erf(g_1[0] + g_2_0)
-    G /=erf(g_1[1] - g_2_1) + erf(g_1[1] + g_2_1)
+    err = erf(g_1[0] - g_2_0) + erf(g_1[0] + g_2_0)
+    err /=erf(g_1[1] - g_2_1) + erf(g_1[1] + g_2_1)
     g_2_0 /= n[0]
     g_2_1 /= n[1]
-    return np.sqrt(n_2 / n_1) * np.exp(g_2_0 * g_2_1 * (n_2**2 - n_1**2)) * G - M_num
+    return np.sqrt(n_2 / n_1) * np.exp(g_2_0 * g_2_1 * (n_2**2 - n_1**2)) * err - M_tilde
 
-def get_amplitude(k, n, d, w, I):
-    const = (np.pi/2)**0.75 * np.sqrt(k / (n * w))
-    A = 2*k*np.pi*d / (n*np.sqrt(1 - d**2))
-    B = 0.25 * n
-    G = erf(A + B) - erf(A - B)
-    I_abs = np.exp(A**2 - 0.5*n*A)
-    return np.abs(I) / (const * I_abs * G)
+def get_amplitude(k, n, damp, omega, I_tilde):
+    const = (np.pi/2)**0.75 * np.sqrt(k / (n * omega))
+    e_1 = 2*k*np.pi*damp / (n*np.sqrt(1 - damp**2))
+    e_2 = 0.25 * n
+    err = erf(e_1 + e_2) - erf(e_1 - e_2)
+    I_abs = np.exp(e_1**2 - 0.5*n*e_1)
+    return np.abs(I_tilde) / (const * I_abs * err)
 
 def cost_fun_amplitude(amp, omega, damp, k, n, I_tilde_abs):
     norm = (0.5 * np.pi)**0.75 * amp * np.sqrt(k / (n * omega))
-    A = 2 * k * np.pi * damp / (n * np.sqrt(1 - damp**2))
-    B = 0.25 * n
-    G = erf(A + B) - erf(A - B)
-    I_anl_abs = norm * np.exp(A * (A - 2*B)) * G
+    e_1 = 2 * k * np.pi * damp / (n * np.sqrt(1 - damp**2))
+    e_2 = 0.25 * n
+    err = erf(e_1 + e_2) - erf(e_1 - e_2)
+    I_anl_abs = norm * np.exp(e_1 * (e_1 - 2*e_2)) * err
     return I_anl_abs - I_tilde_abs
 
 def cost_fun_phase(phase, k, I_tilde_arg):
@@ -243,6 +244,6 @@ if __name__ == "__main__":
     print(f'Damping ratio:\n\tTheorethical: {damping_ratio*100}%\n\tIdentified: {damping*100:.4f}%')
 
 # Identify amplitude and phase
-    amp, phi = identifier.identify_amp_pha(damping)
+    amp, phi = identifier.identify_amplitude_phase(damping)
     print(f'Amplitude:\n\tTheorethical: {amplitude}\n\tIdentified: {amp:.2f}')
     print(f'Phase:\n\tTheorethical: {np.rad2deg(pha):.2f} deg\n\tIdentified: {np.rad2deg(phi):.2f} deg')
